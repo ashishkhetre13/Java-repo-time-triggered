b420935d5f444a2595cef207d2052356ed4ef0e5
ashishkhetre13 <142485326+ashishkhetre13@users.noreply.github.com>
2023-08-24 16:16:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! This is my Master Branch.");
+    System.out.println("Hello world! This is my Time triggered.");
   }
 }
